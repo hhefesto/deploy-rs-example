5ffb635cde2339032fca199bf6e6584b07f97d0d
hhefesto <hhefesto@rdataa.com>
2023-11-23 15:48:57 -0600

    Set password and openssh key

--- nixosModules/admin.nix
+++ nixosModules/admin.nix
@@ -3,10 +3,10 @@
 {
   users.users.admin = {
     name = "admin";
-    initialPassword = "1234";
+    hashedPassword = "$6$JuZni5Aqesp.z5yj$KUO2eAgrma2FXDWWIBqGfOSLf65twcIj4SHFiv7MIGRcaHQxx1nZ.vXmyE5MKq0WS7OwyfdEr8D0URhDt161A/";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [ "your SSH public key here goes here" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcDIsto/6GS7XwTl+uVo4ABeRlRjDwAU0HHy8irqLaB hhefesto@olimpo" ];
   };
   security.sudo.wheelNeedsPassword = false;
   nix.trustedUsers = [ "@wheel" ]; # https://github.com/serokell/deploy-rs/issues/25
