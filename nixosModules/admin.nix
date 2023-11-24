3452f371fa0e89ce395acd2d0e0daec81c0a515e
hhefesto <hhefesto@rdataa.com>
2023-11-24 12:50:53 -0600

    Revert to original pass

--- nixosModules/admin.nix
+++ nixosModules/admin.nix
@@ -4,7 +4,8 @@
   users.users.admin = {
     name = "admin";
     # hashedPassword = "$6$JuZni5Aqesp.z5yj$KUO2eAgrma2FXDWWIBqGfOSLf65twcIj4SHFiv7MIGRcaHQxx1nZ.vXmyE5MKq0WS7OwyfdEr8D0URhDt161A/";
-    initialPassword = "thisPasswordIsPublicButDontPublish";
+    # initialPassword = "thisPasswordIsPublicButDontPublish";
+    initialPassword = "1234";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcDIsto/6GS7XwTl+uVo4ABeRlRjDwAU0HHy8irqLaB hhefesto@olimpo" ];
